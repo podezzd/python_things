8e94184d9d7876caf99395c88a67c41fb023bd89
Родион <84740728+podezzd@users.noreply.github.com>
2024-05-26 21:59:19 +0500

    Update config.py

--- tgbot/config.py
+++ tgbot/config.py
@@ -1,4 +1,4 @@
-token = '6519348024:AAHwft93g7BqJea-XQEbDKy-zH0p0jJ4Aos'
-enemy_token = '7099473855:AAGz-w4mY9ftZJf4d1i3xX3DvkR4wx_m6Q8'
-bebra_token = '6273098904:AAFIuTQmZGwyvSEe4LL-hL97lBbrVKqVEYQ'
+token = "you'll"
+enemy_token = 'never'
+bebra_token = 'see him'
 
